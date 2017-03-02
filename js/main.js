2c41c731e5ae6520776a271b9dcef0d45a59aec0
pkatsaboulas <pkatsaboulas@pkatsaboulas.hightower192.com>
2017-03-02 12:03:36 -0600

    add shadow ot header on scroll

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 $(function(){
 	
 	
+	function scrollStuff(){
+		var $top = $(window).scrollTop();
+
+		if( $top > 60 ){
+			$('header').addClass('active');
+		} else{
+			$('header').removeClass('active');
+		}
+	}
+
+	$(window).on('load', function(){
+
+		scrollStuff();
+
+	});
+
+	$(window).on('resize', function(){
+
+		scrollStuff();
+
+	});
+
+	$(window).on('scroll', scrollStuff);
 	
 	
 });
