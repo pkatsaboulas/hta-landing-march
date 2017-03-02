8902945bc19c29b63084868998a95e0bfc8ecba0
pkatsaboulas <pkatsaboulas@pkatsaboulas.hightower192.com>
2017-03-02 13:24:42 -0600

    header

--- js/main.js
+++ js/main.js
@@ -2,13 +2,9 @@ $(function(){
 	
 	
 	function scrollStuff(){
+		
 		var $top = $(window).scrollTop();
 
-		if( $top > 60 ){
-			$('header').addClass('active');
-		} else{
-			$('header').removeClass('active');
-		}
 	}
 
 	$(window).on('load', function(){
